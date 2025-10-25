55fa0faecba361d87a8ae92feee6c900fc85fce6
maxwellpalance <maxwellpalance@gmail.com>
2025-10-25 15:20:36 -0700

    Most up to date vision code, all connections worked after I added imu to the bus.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/AprilTagLimelightTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Subsystems.Vision;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.Util.Pose;
+
+@TeleOp
+public class AprilTagLimelightTest extends OpMode{
+
+    private Limelight3A limelight;
+    private IMU imu;
+    private double distance;
+
+    @Override
+    public void init(){
+        limelight = hardwareMap.get(Limelight3A.class, "limelight-camera");
+        limelight.setPollRateHz(100);
+        limelight.pipelineSwitch(3); // 2-6 are the pipeline options
+        imu = hardwareMap.get(IMU.class, "imu"); //Don't know its imu
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+
+    }
+
+    @Override
+    public void start(){
+        limelight.start();
+    }
+
+    @Override
+    public void loop(){
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
+
+        LLResult llResult = limelight.getLatestResult();
+        if (llResult != null && llResult.isValid()) {
+            Pose3D botPose = llResult.getBotpose_MT2();
+            telemetry.addData("Target X", llResult.getTx());
+            telemetry.addData("Target Y", llResult.getTy());
+            telemetry.addData("Target Area", llResult.getTa());
+            telemetry.addData("Botpose", botPose.toString());
+        }
+    }
+}
