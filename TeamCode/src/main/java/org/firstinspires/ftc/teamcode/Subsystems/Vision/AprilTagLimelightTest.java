e6a75ac67be515675cb1da32df509611b8223f98
maxwellpalance <maxwellpalance@gmail.com>
2025-11-22 00:19:44 -0800

    Helper functions for Wynn. Need to work on relocalization with him.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/AprilTagLimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/AprilTagLimelightTest.java
@@ -7,23 +7,28 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
 
+import static org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.MM;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.Util.Pose;
 
 @TeleOp
-public class AprilTagLimelightTest extends OpMode{
+public class AprilTagLimelightTest extends OpMode {
 
     private Limelight3A limelight;
     private IMU imu;
     private double distance;
+    private double botXmm;
+    private double botYmm;
+    private double botHeadingDeg;
 
     @Override
-    public void init(){
+    public void init() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight-camera");
         limelight.setPollRateHz(100);
-        limelight.pipelineSwitch(4); //4 is all tags combined into one pipeline.
+        limelight.pipelineSwitch(4);
+        //4 is all tags combined into one pipeline.
         imu = hardwareMap.get(IMU.class, "imu");
         RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
         imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
@@ -31,30 +36,49 @@ public class AprilTagLimelightTest extends OpMode{
     }
 
     @Override
-    public void start(){
+    public void start() {
         limelight.start();
     }
 
     @Override
-    public void loop(){
+    public void loop() {
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         limelight.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
 
         LLResult llResult = limelight.getLatestResult();
         if (llResult != null && llResult.isValid()) {
             Pose3D botPose = llResult.getBotpose_MT2();
+
+            botXmm = botPose.getPosition().x;
+            botYmm = botPose.getPosition().y;
+            botHeadingDeg = botPose.getOrientation().getYaw();
+
             distance = getDistanceFromTags(llResult.getTa());
             telemetry.addData("Distance", distance);
             telemetry.addData("Target X", llResult.getTx());
             telemetry.addData("Target Y", llResult.getTy());
             telemetry.addData("Target Area", llResult.getTa());
             telemetry.addData("Botpose", botPose.toString());
-            telemetry.addData("Yaw", botPose.getOrientation().getYaw());
+            telemetry.addData("X (mm)", botXmm);
+            telemetry.addData("Y (mm)", botYmm);
+            telemetry.addData("Heading (deg)", botHeadingDeg);
         }
     }
+
     public double getDistanceFromTags(double ta) {
         double scale = 17537.71;
-        double distance = (scale / ta);
-        return distance;
+        return scale / ta;
+    }
+
+    public double getBotXmm() {
+        return botXmm;
+    }
+
+    public double getBotYmm() {
+        return botYmm;
+    }
+
+    public double getBotHeadingDeg() {
+        return botHeadingDeg;
     }
 }
